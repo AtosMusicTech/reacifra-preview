0d53393b64bdbb8c745b612f38241f16f8111952
necojr <necoengenheiro@gmail.com>
2024-07-05 13:06:02 -0300

    feat: ajustes gerais

--- src/cifra/CifraView.js
+++ src/cifra/CifraView.js
@@ -7,9 +7,41 @@ import NotesCollection from '../note/NoteCollection';
 import "./CifraView.css";
 
 export default class CifraView extends PiComponent {
-    view = /*html*/`<div class="cifra-view"><div class="titulo">{@_titulo}</div><div id="marcador" class="marcador"></div><pre id="content"></pre></div>`;
+    view = /*html*/`<div :class="cifra-view {@_mostrarAcordes ?  '' : 'no-chords'}">
+        <div class="titulo">
+            {@_titulo}
+        </div>
+
+        <div class="options">
+            <div class="form-check form-switch">
+                <input class="form-check-input" type="checkbox" id="mostrarAcordes" @change="_saveMostrarAcordes($value)" :checked="{@_mostrarAcordes}">
+                <label class="form-check-label" for="mostrarAcordes">Mostrar Acordes</label>
+            </div>
+
+            <div class="form-check form-switch">
+                <input class="form-check-input" type="checkbox" id="barraRolagem" @change="_saveRolagemAutomatica($value)" :checked="{@_rolagemAutomatica}">
+                <label class="form-check-label" for="barraRolagem">Barra de Rolagem Automática</label>
+            </div>
+
+            <div class="form-check form-switch">
+                <input class="form-check-input" type="checkbox" id="evidenciar" @change="_saveEvidenciarNotas($value)" :checked="{@_evidenciarAcordes}">
+                <label class="form-check-label" for="evidenciar">Evidênciar acordes</label>
+            </div>
+        </div>
+
+        <div id="marcador" class="marcador"></div>
+        
+        <pre id="content"></pre>
+    </div>`;
 
     _titulo = '';
+    _mostrarAcordes = true;
+    _rolagemAutomatica = false;
+    _evidenciarAcordes = true;
+
+    viewDidLoad() {
+        this._loadConfig();
+    }
 
     setStream(stream) {
         stream.onNewNote(note => {
@@ -29,6 +61,7 @@ export default class CifraView extends PiComponent {
 
         this._loadNotes();
         this._loadMarcador();
+        this._loadConfig();
 
         this.setMarcadorPosicao(0);
         if (fn) fn();
@@ -36,6 +69,32 @@ export default class CifraView extends PiComponent {
 
     setMarcadorPosicao(position) {
         this.marcador.move(this.notes.getNoteByPosition(position));
+    }
+
+    _saveMostrarAcordes(v) {
+        this._mostrarAcordes = v;
+
+        this._saveConfig();
+    }
+
+    _saveRolagemAutomatica(v) {
+        this._rolagemAutomatica = v;
+
+        if (this.marcador) {
+            this.marcador.setRolagemAutomatica(v);
+        }
+
+        this._saveConfig();
+    }
+
+    _saveEvidenciarNotas(v) {
+        this._evidenciarAcordes = v;
+
+        if (this.marcador) {
+            v ? this.marcador.show() : this.marcador.hide();
+        }
+
+        this._saveConfig();
     }
 
     _formatNotes(cifra) {
@@ -56,6 +115,40 @@ export default class CifraView extends PiComponent {
 
     _loadNotes() {
         this.notes = new NotesCollection(this.$element.find("#content").find('b'));
+    }
+
+    _saveConfig() {
+        window.localStorage.setItem('mostrar_acordes', this._mostrarAcordes);
+        window.localStorage.setItem('rolagem_automatica', this._rolagemAutomatica);
+        window.localStorage.setItem('evidenciar_acordes', this._evidenciarAcordes);
+    }
+
+    _loadConfig() {
+        const config = this._getConfig();
+
+        this._saveMostrarAcordes(config.mostrarAcordes);
+        this._saveRolagemAutomatica(config.rolagemAutomatica);
+        this._saveEvidenciarNotas(config.evidenciarAcordes);
+    }
+
+    _getConfig() {
+        const mostrarAcordes = window.localStorage.getItem('mostrar_acordes');
+        const rolagemAutomatica = window.localStorage.getItem('rolagem_automatica');
+        const evidenciarAcordes = window.localStorage.getItem('evidenciar_acordes');
+
+        if (mostrarAcordes === null || rolagemAutomatica === null || evidenciarAcordes === null) {
+            this._saveConfig();
+
+            mostrarAcordes = true;
+            rolagemAutomatica = true;
+            evidenciarAcordes = true;
+        }
+
+        return {
+            mostrarAcordes: mostrarAcordes === 'true',
+            rolagemAutomatica: rolagemAutomatica === 'true',
+            evidenciarAcordes: evidenciarAcordes === 'true'
+        }
     }
 }
 
--- src/marcador/Marcador.js
+++ src/marcador/Marcador.js
@@ -3,6 +3,7 @@ export default class Marcador {
     constructor($marcador) {
         this.$marcador = $marcador;
         this.note = null;
+        this.rolagemAutomatica = true;
     }
 
     move(note) {
@@ -11,8 +12,7 @@ export default class Marcador {
         const position = note.position();
         const w = note.width();
         const h = note.height();
-
-        this.$marcador.show();
+        
         this.$marcador.css('top', position.top - 2).css('left', position.left - 7);
         this.$marcador.css('width', w + 13).css('height', h + 4);
 
@@ -32,7 +32,23 @@ export default class Marcador {
         }, 200);
     }
 
+    setRolagemAutomatica(b) {
+        this.rolagemAutomatica = b;
+    }
+
+    show() {
+        this.$marcador.show();
+    }
+
+    hide() {
+        this.$marcador.hide();
+    }
+
     _moveScroll() {
+        if (!this.rolagemAutomatica) {
+            return;
+        }
+
         const rect = this.$marcador.get(0).getBoundingClientRect();
         if (rect.bottom > window.innerHeight) {
             window.scrollBy(0, rect.bottom - window.innerHeight + 100);
